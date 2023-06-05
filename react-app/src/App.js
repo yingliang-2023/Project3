bdce908e680ece7ad236ee2926921f230cf95d7f
YING LIANG <y.liang.168@westcliff.edu>
2023-06-04 23:26:00 -0500

    commit 2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Home from './Home';
+import {Router, Routes, Route} from 'react-router-dom';
+import inventoryList from './InventoryList';
+import InventoryEdit from './InventoryEdit';
+import AppNavbar from './Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import InventoryList from './InventoryList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' exact element = {<Home />} />
+          <Route path='/inventories' exact element = {<InventoryList />} />
+          <Route path='/inventories/:id' element = {<InventoryEdit />} />
+        </Routes>
+      </Router>
+    )
+  }
 }
-
 export default App;
--- react-app/src/Navbar.js
+++ react-app/src/Navbar.js
@@ -3,4 +3,16 @@ import {Navbar, NavbarBrand} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+class AppNavbar extends Component {
+    render() {
+        return (
+            <Navbar className='navbar-dark bg-dark px-5' expand="md">
+                <NavbarBrand tag={Link} to ="/">Home</NavbarBrand>
+            </Navbar>
+        )
+    }
+};
+
+export default AppNavbar;
+
 
